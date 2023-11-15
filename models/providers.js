711b2ed3a3108b82cd58f1613c3eb952d0454f57
ASKshatreeya06 <singhprakash545@gmail.com>
2023-11-15 21:27:41 +0530

    version1.0

---
+++ models/providers.js
@@ -0,0 +1,54 @@
+var providers = [
+
+ {
+"id":1,
+"Name": "Jeff",
+"Zip": "55422",
+"Age": 54,
+"Pay": 9587.23,
+"DepartmentId": 1,
+"GetTaxForm": "1099"
+},
+
+{
+"id":2,
+"Name": "Dave",
+"Zip": "03456",
+"Age": 41,
+"Pay": 8547.55,
+"DepartmentId": 1,
+"GetTaxForm": "W2"
+},
+
+{
+"id":3,
+"Name": "Amber",
+"Zip": "41908",
+"Age": 35,
+"Pay": 4878.1,
+"DepartmentId": 2,
+"GetTaxForm": "W2"
+},
+
+{
+"id":4,
+"Name": "Cassie",
+"Zip": "91820",
+"Age": 28,
+"Pay": 4500,
+"DepartmentId": 1,
+"GetTaxForm": "1099"
+},
+
+{
+"id":5,
+"Name": "Albert",
+"Zip": "54321",
+"Age": 39,
+"Pay": 5874.09,
+"DepartmentId": 2,
+"GetTaxForm": "1099"
+}
+]
+
+module.exports = providers;
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+const mainController = require('../controllers/main');
+
+
+/* GET home page. */
+router.get('/', mainController.home);
+
+router.get('/about', mainController.about);
+
+router.get('/contact', mainController.contact);
+router.get('/login', mainController.login);
+router.get('/forgot-password', mainController.forgotpassword);
+router.get('/register', mainController.register);
+
+module.exports = router;
